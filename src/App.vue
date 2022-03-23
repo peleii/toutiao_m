85ff1976f18944e766aabf354b54ddc8e9dae68b
pele <pele@163.com>
2022-03-23 17:58:06 +0800

    导入素材及适配

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
+    <h1>昨日头条</h1>
+    <i class="toutiao toutiao-shouye"></i>
+    <i class="toutiao toutiao-shouji"></i>
+
+    <router-view></router-view>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
 
   </div>
 </template>
+<script>
+export default {
 
+}
+</script>
 <style lang="less">
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/style/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 动态加载设置rem基准值
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
